b7d69b3aa49e5ca0ae880d7e0a7bae4628ca7026
Alexander <evening.sasha@mail.ru>
2023-12-09 23:17:10 +0300

    test2

---
+++ src/02/02.tsx
@@ -0,0 +1,27 @@
+const student = {
+    id: 1,
+    name: "Ivan",
+    age: 23,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+    ]
+}
