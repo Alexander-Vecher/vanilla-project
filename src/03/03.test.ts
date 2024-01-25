80ba57713f5d652e70816b714aa4c151467515cb
Alexander <evening.sasha@mail.ru>
2024-01-25 21:02:52 +0300

    test3

--- src/03/03.test.ts
+++ src/03/03.test.ts
@@ -1,6 +1,71 @@
-import {student} from "../02/02";
+import { StudentType, CityType} from "../02/02_02";
+import {addSkill, changeBudget, changeStudentLive, makeStudentActive} from "./03";
 
-('new tech skill should be added to student', () => {
+let student: StudentType
+let city: CityType
+    beforeEach(()=> {
+        student= {
+            id: 1,
+            name: "Ivan",
+            age: 23,
+            isActive: false,
+            address: {
+                streetTitle: "Surganova 2",
+                city: {
+                    title: "Minsk",
+                    countryTitle: "Belarus"
+                }
+            },
+            technologies: [
+                {
+                    id: 1,
+                    title: "HTML"
+                },
+                {
+                    id: 2,
+                    title: "CSS"
+                },
+                {
+                    id: 3,
+                    title: "React"
+                }
+            ]
+        }
+
+city = {
+    title: 'New York',
+    houses: [
+        {
+            buildedAt: 2012, repaired: false,
+            address: {number: 100, street: {title: "White street"}}
+        },
+
+        {
+            buildedAt: 2008, repaired: false,
+            address: {number: 100, street: {title: "Happy street"}}
+        },
+
+        {
+            buildedAt: 2020, repaired: false,
+            address: {number: 200, street: {title: "Hogwarts street"}}
+        }
+    ],
+
+    governmentBuildings: [
+        {
+            type: "HOSPITAL", budget: 200000, staffCount: 200,
+            address: {street: {title: "Central Str"}}
+        },
+
+        {
+            type: "FIRE-STATION", budget: 500000, staffCount: 1000,
+            address: {street: {title: "Souths park str"}}
+        }
+    ],
+    citizensNumber: 1000000
+}})
+
+test('new tech skill should be added to student', () => {
     expect(student.technologies.length).toBe(3)
     addSkill(student, 'JS')
     expect(student.technologies.length).toBe(4)
@@ -8,13 +73,13 @@ import {student} from "../02/02";
     expect(student.technologies[3].id).toBeDefined()
 })
 
-test.skip('student should be made active', () => {
+test('student should be made active', () => {
     expect(student.isActive).toBe(false)
     makeStudentActive(student)
     expect(student.isActive).toBe(true)
 })
 
-test.skip('does student live in city', () => {
+test('does student live in city', () => {
     expect(student.address.city.title).toBe('Minsk')
     const result1 = changeStudentLive(student, 'Moscow')
     const result2 = changeStudentLive(student, 'Minsk')
@@ -22,29 +87,29 @@ test.skip('does student live in city', () => {
     expect(result2).toBe(true)
 })
 
-test.skip('Budget should be changed for HOSPITAL', () => {
+test('Budget should be changed for HOSPITAL', () => {
     expect(city.governmentBuildings[0].budget).toBe(200000)
-    changeBudget(city, 100000)
+    changeBudget(city.governmentBuildings[0], 100000)
     expect(city.governmentBuildings[0].budget).toBe(300000)
 })
 
-test.skip('Budget should be changed for FIRE-STATION', () => {
+test('Budget should be changed for FIRE-STATION', () => {
     expect(city.governmentBuildings[1].budget).toBe(500000)
-    changeBudget(city, -100000)
+    changeBudget(city.governmentBuildings[1], -100000)
     expect(city.governmentBuildings[1].budget).toBe(400000)
 })
 
-test.skip('House should be repaired', () => {
+test('House should be repaired', () => {
     repairHouse(city)
     expect(city.houses[1].repaired).toBeTruthy()
 })
 
-test.skip('staff should be increased', () => {
+test('staff should be increased', () => {
     toFireStaff(city, 20)
     expect(city.governmentBuildings[0].staffCount).toBe(180)
 })
 
-test.skip('staff should be hire', () => {
+test('staff should be hire', () => {
     toHireStaff(city, 20)
     expect(city.governmentBuildings[0].staffCount).toBe(220)
 })
--- src/03/03.ts
+++ src/03/03.ts
@@ -1,6 +1,22 @@
-import {StudentType} from "../02/02_02";
+import {GoverType, StudentType} from "../02/02_02";
 
 
-export const addSkill=(student:StudentType, skill:sring)=>{
+export const addSkill=(student:StudentType, skill:string)=>{
+student.technologies.push({
+    id: new Date().getTime(),
+    title:skill}
+)
+}
+
+export function makeStudentActive(st:StudentType) {
+    st.isActive=true;
+}
+
+export const changeStudentLive=(st:StudentType, cityName:string)=>{
+  return st.address.city.title===cityName
+}
+
+export const changeBudget=(building:GoverType, money:number)=>{
+    return building.budget+=money;
 
 }
